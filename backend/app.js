8063b43c4ad37dedbd0aae0939a44447917b997a
Adam Wolcott Smith <adamwolcottsmith@gmail.com>
2021-01-12 13:42:56 -0500

    installed mongoose and connected MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
+//MONGODB U/N: hotSaucy PASS: qazQAZ234
+//MONGODB CONNECTION: mongodb+srv://hotSaucy:<password>@cluster0.s9rpb.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://hotSaucy:qazQAZ234@Cluster0.s9rpb.mongodb.net/Cluster0?retryWrites=true&w=majority').then(() => {
+ console.log('Successfully connected to MongoDB Atlas');
+}).catch((error) => {
+ console.log('Unable to connect to MongoDB Atlas');
+ console.error(error)
+})
 
 //CORS
 
