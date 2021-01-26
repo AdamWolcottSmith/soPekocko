7e4fe25a9934e6146f07481aebcbbc4ddc3e49b2
Adam Wolcott Smith <adamwolcottsmith@gmail.com>
2021-01-26 12:09:14 -0500

    created controller

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -15,15 +15,3 @@ const sauceSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
-
-// ● userId: String — the MongoDB unique identifier for the user who created the sauce
-// ● name: String — name of the sauce
-// ● manufacturer: String — manufacturer of the sauce
-// ● description: String — description of the sauce
-// ● mainPepper: String — the main pepper ingredient in the sauce
-// ● imageUrl: String — the URL for the picture of the sauce uploaded by the user
-// ● heat: Number — number between 1 and 10 describing the sauce
-// ● likes: Number — number of users liking the sauce
-// ● dislikes: Number — number of users disliking the sauce
-// ● usersLiked: [String] — array of user IDs of users having liked the sauce
-// ● usersDisliked: [String] — array of user IDs of users having disliked the sauce
